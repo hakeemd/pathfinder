21d2f2a0c13feb48266e49c081e3744264e06a24
yohanakanisia <yohanakanisia@gmail.com>
2017-05-12 16:13:13 +1000

    edit js

--- jsquiz.js
+++ jsquiz.js
@@ -1,24 +1,45 @@
 (function() {
   var questions = [{
-    question: "What is 2*5?",
-    choices: [2, 5, 10, 15, 20],
-    correctAnswer: 2
+    question: "Do you prefer practical or theoretical courses?",
+    choices: ["Practical", "Theoretical"],
+    //correctAnswer: 2
   }, {
-    question: "What is 3*6?",
-    choices: [3, 6, 9, 12, 18],
-    correctAnswer: 4
+    question: "Do you have a lot of fantasy or are you more a realistic person?",
+    choices: ["Realistic", "Fantasy"],
+    //correctAnswer: 4
   }, {
-    question: "What is 8*9?",
-    choices: [72, 99, 108, 134, 156],
-    correctAnswer: 0
+    question: "How often do you experience problems with bumping into things, losing stuff or other unimportant problems related to physical awareness?",
+    choices: ["Sometimes", "Quite often"],
+    //correctAnswer: 0
   }, {
-    question: "What is 1*7?",
-    choices: [4, 5, 6, 7, 8],
-    correctAnswer: 3
+    question: "Which of the following best describes you when it comes to making decisions?",
+    choices: ["I often make snap decisions and rarely think them through.", "I’m less likely to make rash decisions and would rely heavily on my intuition, feeling and reactions in making a decision."],
+    //correctAnswer: 3
   }, {
-    question: "What is 8*8?",
-    choices: [20, 30, 40, 50, 64],
-    correctAnswer: 4
+    question: "Which of the following best describes you during a group discussion or group decision    making?",
+    choices: ["I gravitate toward groups and constant action, and I tend to think out loud and mostly vocal with my opinions.", "I tend to prefer quiet solitude, time to think before speaking (or acting), and building relationships and trust one-on-one."],
+    //correctAnswer: 4
+  }, {
+      question: "I mostly enjoy,",
+      choices: ["Personal interactions, social gatherings, and sharing my ideas effectively before making decisions.", "Reflection and deep dives into my own inner landscape to research ideas, and I tend to focus deeply on work."],
+  }, {
+      question: "Do you like to volunteer or involve yourself in new activities offered by your workplace?",
+      choices: ["Yes, I love to volunteer!", "If there’s someone else who can do it, I don’t see why I should."],
+  }, {
+      question: "Which of the following best describes you?",
+      choices: ["I often involve myself in those group chats my co-workers set up, even if its just for the sake of making jokes with my co-workers.", "I rarely reply to the group chats unless very necessary."],
+  }, {
+      question: "Before I start the day at work, I tend to..",
+      choices: ["Ease into the workday by sorting and planning alone for the first half an hour.", "Rather meet with people and start the day off running. I’m more productive when I can bounce ideas off others during the workday."]
+  }, {
+      questions: "If you are walking on a crowded street, which one do you tend to do?",
+      choices: ["Paying attention to your phone as you walk.", "Walk casually and look at your surroundings."]
+  }, {
+      questions: "Which one best describes yourself when you are in a large gathering or party?",
+      choices: ["Make your own space for yourself.", "Talk to someone around you."]
+  }, {
+      questions: "You will work effectively when ...",
+      choices: ["You are in a quiet environment.", "You are in a space full of people / noise."]
   }];
   
   var questionCounter = 0; //Tracks question number
